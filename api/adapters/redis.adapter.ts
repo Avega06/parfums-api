db9240bdfceda1f9e6bc86f78089e5e30a466205
Alex Vega <alexvega06.1996@gmail.com>
2025-06-25 06:18:45 -0400

    fix redis adapter

--- api/adapters/redis.adapter.ts
+++ api/adapters/redis.adapter.ts
@@ -1,27 +1,27 @@
-// import { createClient, RedisClientType } from "redis";
+import { createClient, RedisClientType } from "redis";
 
-// let client: RedisClientType | null = null;
+let client: RedisClientType | null = null;
 
-// export const getClientConnection = async (): Promise<RedisClientType> => {
-//   if (client) return client;
+export const getClientConnection = async (): Promise<RedisClientType> => {
+  if (client) return client;
 
-//   if (!process.env.REDIS_MODE) {
-//     throw new Error("Redis - Mode not found");
-//   }
+  if (!process.env.REDIS_MODE) {
+    throw new Error("Redis - Mode not found");
+  }
 
-//   if (!process.env.REDIS_HOST_URL_PROD && !process.env.REDIS_LOCAL_HOST) {
-//     throw new Error("Redis - URL not found");
-//   }
+  if (!process.env.REDIS_HOST_URL_PROD && !process.env.REDIS_LOCAL_HOST) {
+    throw new Error("Redis - URL not found");
+  }
 
-//   client = createClient({
-//     url: process.env.REDIS_HOST_URL ?? process.env.REDIS_LOCAL_HOST,
-//   });
+  client = createClient({
+    url: process.env.REDIS_HOST_URL_PROD ?? process.env.REDIS_LOCAL_HOST,
+  });
 
-//   client.on("error", (err) => {
-//     console.error("Redis error:", err);
-//   });
+  client.on("error", (err) => {
+    console.error("Redis error:", err);
+  });
 
-//   await client.connect();
+  await client.connect();
 
-//   return client;
-// };
+  return client;
+};
