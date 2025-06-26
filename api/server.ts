2fba20fc31c43e8e8ce378352c8360fab936f1ee
Alex Vega <alexvega06.1996@gmail.com>
2025-06-26 05:05:12 -0400

    add multiple orrigins

--- api/server.ts
+++ api/server.ts
@@ -9,10 +9,22 @@ import { handle } from "hono/vercel";
 
 const port = Bun.env.PORT ?? 3000;
 const app = new Hono();
+const allowedOrigins = [
+  "https://f3ghhw6s-4200.brs.devtunnels.ms",
+  "http://localhost:4200",
+  "http://127.0.0.1:4200",
+];
+
 app.use(
   "*",
   cors({
-    origin: "http://parfums-app-murex.vercel.app",
+    origin: (origin, c) => {
+      if (!origin) return "*";
+      if (allowedOrigins.includes(origin)) {
+        return origin; // permitir este origen
+      }
+      return null; // bloquear otros
+    },
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
